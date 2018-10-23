5b64cc66b07059c031a6af078e5c6520afb5057b
Mateo Doslic <mateo55@live.com>
2018-10-23 19:49:07 +0200

    Initial Commit

---
+++ UHP zadatak/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UHP zadatak
+//
+//  Created by Mateo Doslic on 23/10/2018.
+//  Copyright © 2018 Mateo Doslic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
