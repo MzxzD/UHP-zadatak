5c4ef7bdb2180ae3b52ee91482212257b2d4fb00
Mateo Doslic <mateo55@live.com>
2018-10-24 13:37:40 +0200

    UI partially implemented

---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+def shared_pods
+  pod 'Alamofire'
+  pod 'Kingfisher'
+  pod 'RxSwift'
+  pod 'RxCocoa'
+  pod 'R.swift','~> 5.0.0.alpha'
+end
+
+target 'UHP zadatak' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  shared_pods
+  # Pods for UHP zadatak
+
+end
--- UHP zadatak/Home/UI/HomeViewController.swift
+++ UHP zadatak/Home/UI/HomeViewController.swift
@@ -7,12 +7,158 @@
 //
 
 import UIKit
+import Alamofire
+import RxSwift
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    
+    let randomArray = ["blag", "Blah", "wah?"]
+    
+    let picker = UIPickerView()
+    var activeTextField: UITextField!
+    
+    var inputTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Input Value"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    var fromValueTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "FROM value"
+        return textField
+    }()
+    
+    var toValueTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "To value"
+        return textField
+    }()
+    
+    var resultLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Result"
+        return label
+    }()
+    let calculateButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 50, y: 50, width: 100, height: 50))
+        button.setTitle("Calculate", for: .normal)
+        button.backgroundColor = .blue
+        //        button.setImage(#imageLiteral(resourceName: "search_icon"), for: .normal)
+        //        button.addTarget(self, action: #selector(searchTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+        
+        
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
-    }
 
+        setupView()
+        self.title = "UHP Zadatak"
+    }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        activeTextField = textField
+        switch textField {
+        case fromValueTextField:
+            print("fromFieldSelected")
+        case toValueTextField:
+            print("ToFieldSelected")
+        default:
+            print("Default")
+        }
+        
+        picker.reloadAllComponents()
+        return true
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return randomArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return randomArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(randomArray[row])
+        activeTextField.text = randomArray[row]
+    }
+    
+    func createToolbar() {
+        let toolbar = UIToolbar()
+        toolbar.barStyle = .default
+        toolbar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(doneClicked))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(cancelClicked))
+        
+        toolbar.setItems([doneButton, spaceButton, cancelButton], animated: true)
+        
+        fromValueTextField.inputAccessoryView = toolbar
+        toValueTextField.inputAccessoryView = toolbar
+    }
+    
+    
+    @objc func doneClicked() {
+        activeTextField.resignFirstResponder()
+    }
+    
+    @objc func cancelClicked() {
+        activeTextField.text = .empty
+        activeTextField.resignFirstResponder()
+    }
+    
+    
+    private func setupView() {
+        view.backgroundColor = .white
+        
+        fromValueTextField.delegate = self
+        toValueTextField.delegate = self
+        picker.delegate = self
+        picker.dataSource = self
+        
+        fromValueTextField.inputView = picker
+        toValueTextField.inputView = picker
+        
+        createToolbar()
+        
+        self.view.addSubview(fromValueTextField)
+        fromValueTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        fromValueTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8).isActive = true
+        fromValueTextField.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        self.view.addSubview(toValueTextField)
+        toValueTextField.topAnchor.constraint(equalTo: fromValueTextField.topAnchor).isActive = true
+        toValueTextField.leadingAnchor.constraint(equalTo: fromValueTextField.trailingAnchor, constant: 10).isActive = true
+        toValueTextField.trailingAnchor.constraint(equalTo: view!.trailingAnchor, constant: -8).isActive = true
+        toValueTextField.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        self.view.addSubview(inputTextField)
+        inputTextField.topAnchor.constraint(equalTo: fromValueTextField.bottomAnchor, constant: 8).isActive = true
+        inputTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+//        inputTextField.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        self.view.addSubview(calculateButton)
+        calculateButton.topAnchor.constraint(equalTo: inputTextField.bottomAnchor, constant: 16).isActive = true
+        calculateButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        
+        self.view.addSubview(resultLabel)
+        resultLabel.topAnchor.constraint(equalTo: calculateButton.bottomAnchor, constant: 8).isActive = true
+        resultLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    
 }
